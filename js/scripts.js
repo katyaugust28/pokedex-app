e17e3efd12489aa63a524e968a538d4fa3546123
katyaugust28 <katyaugust28@gmail.com>
2021-05-25 11:44:01 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+alert('Hello world');
+
+/* The text below when opened in a browser sits Bob and John Doe side by side
+like this "BobJohn Doe"*/
+// let myName="Bob";
+// document.write(myName);
+// myName="John Doe";
+// document.write(myName);
+
+// let simpleAddition= 2 + 2;
+// document.write(simpleAddition);
+// document.write(" ");
+//
+// let size= 100;
+// let doubleSize = size * 2;
+// document.write(doubleSize);
+// document.write(" ");
+//
+// let minSize= (doubleSize * 2)- (size / 2)
+// document.write(minSize);
+// document.write(" ");
+
+let favoriteFood= "sushi";
+document.write("My favorite food is ")
+document.write(favoriteFood);
+document.write(". ");
