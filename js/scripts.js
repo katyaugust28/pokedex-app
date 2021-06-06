4f4eb4ac3c64f05875624e7d442cdc6e84d872e9
katyaugust28 <katyaugust28@gmail.com>
2021-06-06 11:50:19 -0700

    created for loop to print each pokemon; created else-if to single out tallest pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,76 @@
 
 let pokemonList= [];
 
-let pokemon1={
+let bulbasaur={
   name: 'Bulbasaur',
   height: 2.25,
   types: ['grass','poison']
 };
 
-let pokemon2={
+let ivysaur={
   name: 'Ivysaur',
   height: 3,
   types: ['grass','poison']
 };
 
-let pokemon3={
+let venusaur={
   name: 'Venusaur',
   height: 6.5,
   types: ['grass','poison']
 };
 
-let pokemon4={
+let charmander={
   name: 'Charmander',
   height: 2,
   types: ['fire']
 };
 
-let pokemon5={
+let charmeleon={
   name: 'Charmeleon',
   height: 3.5,
   types: ['fire']
 };
 
-let pokemon6={
+let charizard={
   name: 'Charizard',
   height: 5.5,
   types: ['fire', 'flying']
 };
 
-let pokemon7={
+let squirtle={
   name: 'Squirtle',
   height: 1.5,
   types: ['water']
 };
 
-let pokemon8={
+let wartortle={
   name: 'Wartortle',
   height: 3.25,
   types: ['water']
 };
 
-let pokemon9={
+let blastoise={
   name: 'Blastoise',
   height: 5.25,
   types: ['water']
 };
 
-pokemonList.push(pokemon1)
-pokemonList.push(pokemon2)
-pokemonList.push(pokemon3)
-pokemonList.push(pokemon4)
-pokemonList.push(pokemon5)
-pokemonList.push(pokemon6)
-pokemonList.push(pokemon7)
-pokemonList.push(pokemon8)
-pokemonList.push(pokemon9)
+pokemonList.push(bulbasaur)
+pokemonList.push(ivysaur)
+pokemonList.push(venusaur)
+pokemonList.push(charmander)
+pokemonList.push(charmeleon)
+pokemonList.push(charizard)
+pokemonList.push(squirtle)
+pokemonList.push(wartortle)
+pokemonList.push(blastoise)
 
 //test on website in the console with console.log(pokemonList)
+
+for (let i=0; i<pokemonList.length; i++){
+  if (pokemonList[i].height> 6){
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")- Wow, that's big! " +"<br>" );
+  } else {
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") "+"<br>");
+  }
+}
