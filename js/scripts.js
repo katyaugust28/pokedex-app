c4b01884b85928d89468b72f8332696c16f0649c
katyaugust28 <katyaugust28@gmail.com>
2021-07-10 12:35:41 -0700

    deleted my original push.pokemon files and original lists

--- js/scripts.js
+++ js/scripts.js
@@ -71,69 +71,7 @@ let pokemonRepository = (function () {
 })();
 
 
-let bulbasaur={
-  name: 'Bulbasaur',
-  height: 2.25,
-  types: ['grass','poison']
-};
 
-let ivysaur={
-  name: 'Ivysaur',
-  height: 3,
-  types: ['grass','poison']
-};
-
-let venusaur={
-  name: 'Venusaur',
-  height: 6.5,
-  types: ['grass','poison']
-};
-
-let charmander={
-  name: 'Charmander',
-  height: 2,
-  types: ['fire']
-};
-
-let charmeleon={
-  name: 'Charmeleon',
-  height: 3.5,
-  types: ['fire']
-};
-
-let charizard={
-  name: 'Charizard',
-  height: 5.5,
-  types: ['fire', 'flying']
-};
-
-let squirtle={
-  name: 'Squirtle',
-  height: 1.5,
-  types: ['water']
-};
-
-let wartortle={
-  name: 'Wartortle',
-  height: 3.25,
-  types: ['water']
-};
-
-let blastoise={
-  name: 'Blastoise',
-  height: 5.25,
-  types: ['water']
-};
-
-pokemonList.push(bulbasaur)
-pokemonList.push(ivysaur)
-pokemonList.push(venusaur)
-pokemonList.push(charmander)
-pokemonList.push(charmeleon)
-pokemonList.push(charizard)
-pokemonList.push(squirtle)
-pokemonList.push(wartortle)
-pokemonList.push(blastoise)
 
 pokemonRepository.loadList().then( function() {
   pokemonRepository.getAll().forEach(function (pokemon) {
